66504749e18e2dc7f1f9bd76bcd78e97f04ee134
ShinSeongCheol <cher3725@naver.com>
2022-03-30 17:45:30 +0900

    servlet configuration and variable

--- jsp&servlet/edu/src/com/edu/test/ServletConfigTestServlet.java
+++ jsp&servlet/edu/src/com/edu/test/ServletConfigTestServlet.java
@@ -9,10 +9,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class ServletConfigTestServlet extends HttpServlet{
+	
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html; charset=UTF-8");
+		
 		PrintWriter out = resp.getWriter();
+		
 		String env = this.getInitParameter("charset");
 		req.setCharacterEncoding(env);
 		out.print("<h3> 이름 : " + req.getParameter("name"));
