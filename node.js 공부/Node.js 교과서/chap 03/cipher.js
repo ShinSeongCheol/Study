7a7cedfb2f154cec90cdbf4fffd1533d1167d4e4
Shinseongcheol <cher3725@naver.com>
2022-05-28 22:27:59 +0900

    encrypt

--- node.js 공부/Node.js 교과서/chap 03/cipher.js
+++ node.js 공부/Node.js 교과서/chap 03/cipher.js
@@ -1,12 +1,13 @@
 const crypto = require('crypto');
 const key = crypto.randomBytes(32);
 const iv = crypto.randomBytes(16);
-const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
-let result = cipher.update('암호화 문장');
-result += cipher.final('base64');
-console.log(result);
 
-const decipher = crypto.createDecipheriv('aes-256-ccm', Buffer.from(key), iv);
-let result2 = decipher.update(result, 'base64');
-result2 += decipher.final('utf8');
-console.log('복호화:', result2);
+function encrypt(text){
+    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+    encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+    return { iv : iv.toString('hex'),
+    encryptedData : encrypted.toString('hex') };
+}
+console.log(encrypt('암호화 문장'));
