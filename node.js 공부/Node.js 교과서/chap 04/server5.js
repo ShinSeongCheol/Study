72f9204a232820b985822488795beb66d29ae788
Shinseongcheol <cher3725@naver.com>
2022-06-03 20:33:09 +0900

    session and cookie

--- node.js 공부/Node.js 교과서/chap 04/server5.js
+++ node.js 공부/Node.js 교과서/chap 04/server5.js
@@ -1,9 +1,9 @@
 const http = require('node:http');
 const fs = require('fs');
 const url = require('node:url');
-const querystring = require('node:querystring');
+const qs = require('node:querystring');
 
-const parseCookies = (cookie = '') =>
+const parseCookies = (cookie = '') => 
     cookie
     .split(';')
     .map(v => v.split('='))
@@ -11,33 +11,39 @@ const parseCookies = (cookie = '') =>
     .reduce((acc, [k, v]) => {
         acc[k.trim()] = decodeURIComponent(v);
         return acc;
-}, {});
+    }, {});
+
+const session = {};
 
 http.createServer((req, res) => {
     const cookies = parseCookies(req.headers.cookie);
-    if(req.url.startsWith('/login')) {
-        const { query } = url.parse(req.url);
-        const { name } = querystring.parse(query);
+    if(req.url.startsWith('/login')){
+        const {query} = url.parse(req.url);
+        const {name} = qs.parse(query);
         const expires = new Date();
         expires.setMinutes(expires.getMinutes() + 5);
+        const randomInt = Date.now();
+        session[randomInt] = {
+            name,
+            expires,
+        };
         res.writeHead(302, {
             Location: '/',
-            'Set-Cookie': `name=${encodeURIComponent(name)};
-            Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
+            'Set-Cookie': `session=${randomInt}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
         });
         res.end();
-    } else if (cookies.name) {
+    }else if (cookies.session && session[cookies.session].expires > new Date()){
         res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-        res.end(`${cookies.name}님 안녕하세요`);
-    } else {
+        res.end(`${session[cookies.session].name}님 안녕하세요`);
+    }else {
         fs.readFile('./server4.html', (err, data) => {
-            if(err) {
+            if(err){
                 throw err;
             }
             res.end(data);
-        })
+        });
     }
 })
-.listen(8083, () => {
-    console.log('8083번 포트에서 서버 대기 중입니다');
+.listen(8084, () => {
+    console.log('8084번 포트에서 서버 대기 중입니다!');
 });
