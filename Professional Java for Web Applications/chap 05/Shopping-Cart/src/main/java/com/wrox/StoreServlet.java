19dd32e8b83fd3fe9cff1c7bce80b4485e30ee1b
Shinseongcheol <cher3725@naver.com>
2022-06-04 21:58:25 +0900

    jsp session study

--- Professional Java for Web Applications/chap 05/Shopping-Cart/src/main/java/com/wrox/StoreServlet.java
+++ Professional Java for Web Applications/chap 05/Shopping-Cart/src/main/java/com/wrox/StoreServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet(
 		name = "storeServlet",
@@ -32,6 +33,64 @@ public class StoreServlet extends HttpServlet{
 		if(action == null)
 			action = "browse";
 		
+		switch(action) {
+		case "addToCart" :
+			this.addToCart(request, response);
+			break;
+			
+		case "viewCart" :
+			this.viewCart(request, response);
+			break;
+			
+		case "browse" :
+			this.browse(request, response);
+			break;
+		case "emptyCart" :
+			this.emptyCart(request, response);
+			break;
+		}
+	}
+	
+	private void viewCart(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("products", this.products);
+		request.getRequestDispatcher("/WEB-INF/jsp/view/viewCart.jsp").forward(request, response);
+	}
+	
+	private void browse(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("products", this.products);
+		request.getRequestDispatcher("/WEB-INF/jsp/view/browse.jsp").forward(request, response);
+	}
+	
+	private void addToCart(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		int productId;
+		
+		try {
+			productId = Integer.parseInt(request.getParameter("productId"));
+		}catch(Exception e) {
+			response.sendRedirect("shop");
+			return;
+		}
+		
+		HttpSession session = request.getSession();
+		
+		if(session.getAttribute("cart") == null) {
+			session.setAttribute("cart", new Hashtable<Integer, Integer>());
+		}
+		
+		@SuppressWarnings("unchecked")
+		Map<Integer, Integer> cart = (Map<Integer, Integer>)session.getAttribute("cart");
+		
+		if(!cart.containsKey(productId)) {
+			cart.put(productId, 0);
+		}
+		cart.put(productId, cart.get(productId) + 1);
+		
+		response.sendRedirect("shop?action=viewCart");
+	}
+	
+	private void emptyCart(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getSession().removeAttribute("cart");
+		response.sendRedirect("shop?action=viewCart");
 	}
 }
