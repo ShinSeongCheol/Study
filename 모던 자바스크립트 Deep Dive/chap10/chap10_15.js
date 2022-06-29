473148cf1319f0b2b42d7808594c536018d49574
Shinseongcheol <cher3725@naver.com>
2022-06-29 13:54:29 +0900

    모던 자바스크립트 Deep Dive chap10

---
+++ 모던 자바스크립트 Deep Dive/chap10/chap10_15.js
@@ -0,0 +1,14 @@
+var person = {
+    'last-name': 'Lee',
+    1: 10
+};
+
+// console.log(person.'last-name');
+// console.log(person.last-name);
+// console.log(person.[last-name]);
+console.log(person['last-name']);
+
+// console.log(person.1);
+// console.log(person.'1');
+console.log(person[1]);
+console.log(person['1']);
---
+++ 모던 자바스크립트 Deep Dive/chap10/chap10_18.js
@@ -0,0 +1,10 @@
+var person = {
+    name: 'Lee'
+};
+
+person.age = 20;
+
+delete person.age;
+delete person.address;
+
+console.log(person);
