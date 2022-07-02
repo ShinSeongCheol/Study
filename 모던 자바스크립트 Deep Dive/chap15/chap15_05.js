99f4ccf2e582a99a2aa8b7dae4e0ea9518079d49
ShinSeongCheol <cher3725@naver.com>
2022-07-02 17:42:15 +0900

    모던 자바스크립트 chap15

---
+++ 모던 자바스크립트 Deep Dive/chap15/chap15_05.js
@@ -0,0 +1,8 @@
+var foo = 123;
+var foo = 456;
+
+let bar = 123;
+let bar = 456;
+
+console.log(foo);
+console.log(bar);
---
+++ 모던 자바스크립트 Deep Dive/chap15/chap15_06.js
@@ -0,0 +1,7 @@
+let foo = 1;
+{
+    let foo = 2;
+    let bar = 3;
+}
+console.log(foo);
+console.log(bar);
