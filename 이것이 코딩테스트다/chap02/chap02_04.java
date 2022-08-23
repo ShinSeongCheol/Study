918135f4a46784b579d0571c77ad54b316ab0394
Shinseongcheol <cher3725@naver.com>
2022-08-23 09:35:14 +0900

    1이 될때까지

---
+++ 이것이 코딩테스트다/chap02/chap02_04.java
@@ -0,0 +1,25 @@
+package chap02;
+
+import java.util.Scanner;
+
+//1이 될때까지
+public class chap02_04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int count = 0;
+
+        while(N != 1) {
+            if(N % K == 0) {
+                N = N / K;
+            }
+            else {
+                N = N - 1;
+            }
+            count++;
+        }
+
+        System.out.println(count);
+    }
+}
