1c22250ad4fa3e658ec36e4cb6ff09f10c2543a9
Shinseongcheol <cher3725@naver.com>
2022-08-24 09:39:22 +0900

    chap03

--- 이것이 코딩테스트다/chap02/chap03_04.java
+++ 이것이 코딩테스트다/chap02/chap03_04.java
@@ -3,7 +3,7 @@ package chap02;
 import java.util.Scanner;
 
 //1이 될때까지
-public class chap02_04 {
+public class chap03_04 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
