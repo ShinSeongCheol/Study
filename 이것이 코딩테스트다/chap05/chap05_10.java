32d27edbc40c5033b869bac90f05ccb1db85121e
ShinSeongCheol <cher3725@naver.com>
2022-08-30 21:32:12 +0900

    이것이 코딩테스트다 - 음료수 얼리기  chap05

--- 이것이 코딩테스트다/chap05/chap05_10.java
+++ 이것이 코딩테스트다/chap05/chap05_10.java
@@ -4,49 +4,47 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class chap05_10 {
-
     private static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
     private static int N, M;
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         M = sc.nextInt();
         sc.nextLine();
 
-        for(int i=0; i<N; i++) {
+        for (int i = 0; i < N; i++) {
             graph.add(new ArrayList<Integer>());
             String str = sc.nextLine();
-            for(int j=0; j<M; j++) {
+            for (int j = 0; j < M; j++) {
                 graph.get(i).add(str.charAt(j) - '0');
             }
         }
 
         int result = 0;
-        for(int i=0; i<=N; i++) {
-            for(int j=0; j<=M; j++) {
-                if(dfs(i, j)){
-                    result ++;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (dfs(i, j)) {
+                    result += 1;
                 }
             }
+            System.out.println();
         }
 
-        for(int i=0; i<N; i++) {
-            System.out.println(graph.get(i));
-        }
         System.out.println(result);
     }
 
     private static boolean dfs(int x, int y) {
-        if (x <= -1 || x >= N || y <= -1 || y >= N) {
+        if (x <= -1 || x >= N || y <= -1 || y >= M) {
             return false;
         }
 
-        if(graph.get(x).get(y) == 0) {
+        if (graph.get(x).get(y) == 0) {
             graph.get(x).set(y, 1);
-            dfs(x-1, y);
-            dfs(x, y-1);
-            dfs(x+1, y);
-            dfs(x, y+1);
+            dfs(x, y + 1);
+            dfs(x, y - 1);
+            dfs(x - 1, y);
+            dfs(x + 1, y);
             return true;
         }
 
