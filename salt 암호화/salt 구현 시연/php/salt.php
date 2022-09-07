5e6453d03ed59785ce82df35cb4b86468012d57c
Shinseongcheol <cher3725@naver.com>
2022-09-07 17:10:25 +0900

    salt 암호화 리팩토링 userInfo 클래스를 MariaDb 클래스에서만 사용하게 바꿈

--- salt 암호화/salt 구현 시연/php/salt.php
+++ salt 암호화/salt 구현 시연/php/salt.php
@@ -11,29 +11,29 @@
             return $result;
         }
         
-        public function encryptPassword($arr, $password) {
+        public function encryptPassword($arr) {
             $count = count($arr);
-            $mod = unpack("I", $password)[1] % $this -> getFactorial($count);
-            
-            for($i1=0; $i1<$count; $i1++) {
-                for($i2=0; $i2<$count; $i2++) {
-                    for($i3=0; $i3<$count; $i3++) {
-                        for($i4=0; $i4<$count; $i4++){
-                            for($i5=0; $i5<$count; $i5++){
-                                if($i1 == $i2 || $i1 == $i3 || $i1 == $i4 || $i1 == $i5
-                                || $i2 == $i3 || $i2 == $i4 || $i2 == $i5
-                                || $i3 == $i4 || $i3 == $i5
-                                || $i4 == $i5
-                                )
-                                continue;
-                                $saltString[] = $arr[$i1].$arr[$i2].$arr[$i3].$arr[$i4].$arr[$i5];
+                $mod = unpack("I", $arr[1])[1] % $this -> getFactorial($count);
+
+                for($i1=0; $i1<$count; $i1++) {
+                    for($i2=0; $i2<$count; $i2++) {
+                        for($i3=0; $i3<$count; $i3++) {
+                            for($i4=0; $i4<$count; $i4++){
+                                for($i5=0; $i5<$count; $i5++){
+                                    if($i1 == $i2 || $i1 == $i3 || $i1 == $i4 || $i1 == $i5
+                                    || $i2 == $i3 || $i2 == $i4 || $i2 == $i5
+                                    || $i3 == $i4 || $i3 == $i5
+                                    || $i4 == $i5
+                                    )
+                                    continue;
+                                    $saltString[] = $arr[$i1].$arr[$i2].$arr[$i3].$arr[$i4].$arr[$i5];
+                                }
                             }
                         }
                     }
                 }
-            }
 
-            return hash('sha256', "$saltString[$mod]");
+                return hash('sha256', "$saltString[$mod]");
         }
     }
 ?>
