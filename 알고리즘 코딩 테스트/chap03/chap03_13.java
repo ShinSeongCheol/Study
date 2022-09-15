0a24f9c579ec523083cf8fd6f77dd9535b50f22e
ShinSeongCheol <cher3725@naver.com>
2022-09-15 21:23:28 +0900

    알고리즘 코딩 테스트 - 카드 게임

---
+++ 알고리즘 코딩 테스트/chap03/chap03_13.java
@@ -0,0 +1,23 @@
+package chap03;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class chap03_13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> queue = new LinkedList<>();
+        int N = sc.nextInt();
+
+        for(int i=1; i<=N; i++)
+            queue.add(i);
+
+        while(queue.size() > 1) {
+            queue.poll();
+            queue.add(queue.poll());
+        }
+
+        System.out.println(queue.poll());
+    }
+}
