97cdbfd85b78c6d9b78828f3dcf948f541d250df
ShinSeongCheol <cher3725@naver.com>
2022-09-18 01:46:59 +0900

    알고리즘 코딩 테스트 내림차순으로 자릿수 정렬하기

---
+++ 알고리즘 코딩 테스트/chap04/chap04_03.java
@@ -0,0 +1,30 @@
+package chap04;
+
+import java.util.Scanner;
+
+public class chap04_03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int[] A = new int[str.length()];
+        for(int i=0; i<str.length(); i++) {
+            A[i] = Integer.parseInt(str.substring(i, i+1));
+        }
+
+        for(int i=0; i<str.length(); i++) {
+            int max = i;
+            for(int j= i + 1; j<str.length(); j++) {
+                if(A[j] > A[max])
+                    max = j;
+            }
+            if(A[i] < A[max]) {
+                int temp = A[i];
+                A[i] = A[max];
+                A[max] = temp;
+            }
+        }
+
+        for(int i : A)
+            System.out.print(i);
+    }
+}
